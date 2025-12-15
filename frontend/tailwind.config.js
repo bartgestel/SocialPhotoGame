bc5763dc02084f61fe9dc2dd36fcc4a491b062a0
Cocovg <Cocovanglabbeek@Hotmail.com>
2025-12-15 11:21:21 +0100

    Update package-lock.json to version 3 and upgrade pnpm to 10.25.0; enhance tailwind.config.js with new theme colors for improved styling.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        primary: "#AF8159",
+        secondary: "#5E3023",
+        tertiary: "#F3E9DC",
+        background: "#F4F4F4",
+        actionButton: "#489059",
+        textPrimary: "#000000",
+        textActionButton: "#FFFFFF",
         extend: {},
     },
     plugins: [],
